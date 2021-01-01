c784b88199fcbc46751f48e483ab440280808bb9
Euiyub Jung <dwywdo@gmail.com>
2021-01-01 16:18:49 +0900

    Initialize Spring Archive for self study

---
+++ Spring/springarchive/src/test/java/com/euiyub/springarchive/SpringarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euiyub.springarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringarchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

2281d2edffb936a8a4be29598b4155a0242c7a5f
Euiyub Jung <dwywdo@gmail.com>
2021-01-02 17:21:01 +0900

    Add SpEL Test code

---
+++ Spring/springarchive/src/main/java/com/euiyub/springarchive/Sample.java
@@ -0,0 +1,16 @@
+package com.euiyub.springarchive;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+    private int data = 200;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}

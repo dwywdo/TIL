6ae789602305e6a91943b5964faff1e7231fd302
Euiyub Jung <dwywdo@gmail.com>
2021-01-11 22:09:12 +0900

    Customize Servlet for Spring Boot

--- Spring/spring-boot-basic-server/src/main/java/me/dwywdo/springbootbasicserver/SpringBootBasicServerApplication.java
+++ Spring/spring-boot-basic-server/src/main/java/me/dwywdo/springbootbasicserver/SpringBootBasicServerApplication.java
@@ -1,44 +1,11 @@
 package me.dwywdo.springbootbasicserver;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-
-public final class SpringBootBasicServerApplication {
-
-    public static void main(String[] args) throws LifecycleException {
-        final Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        tomcat.getConnector();
-
-        final Context context = tomcat.addContext("/", "/");
-
-        final HttpServlet servlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-                    throws ServletException, IOException {
-                final PrintWriter writer = resp.getWriter();
-                writer.println("<html><head><title>");
-                writer.println("Hey, Tomcat");
-                writer.println("</title></head>");
-                writer.println("<body><h1>Hello Tomcat</h1></body>");
-                writer.println("</html>");
-            }
-        };
-
-        final String servletName = "helloServlet";
-        tomcat.addServlet("/", servletName, servlet);
-        context.addServletMappingDecoded("/hello", servletName);
-        tomcat.start();
-        tomcat.getServer().await();
+@SpringBootApplication
+public class SpringBootBasicServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasicServerApplication.class, args);
     }
-
 }

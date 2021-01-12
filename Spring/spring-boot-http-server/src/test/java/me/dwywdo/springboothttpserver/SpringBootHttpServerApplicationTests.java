9b473fe5147a4b13645c9801340b6599c56e65eb
Euiyub Jung <dwywdo@gmail.com>
2021-01-12 21:41:35 +0900

    Use multi-connector for both http/https enabled

---
+++ Spring/spring-boot-http-server/src/test/java/me/dwywdo/springboothttpserver/SpringBootHttpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dwywdo.springboothttpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHttpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f790c90907dff37b44a2b158e549e6ced2d6058f
Euiyub Jung <dwywdo@gmail.com>
2021-01-17 16:09:23 +0900

    Spring Boot @ConfigurationProperties

--- Spring/spring-boot-configuration/src/main/java/me/dwywdo/springbootconfiguration/SampleRunner.java
+++ Spring/spring-boot-configuration/src/main/java/me/dwywdo/springbootconfiguration/SampleRunner.java
@@ -1,5 +1,6 @@
 package me.dwywdo.springbootconfiguration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner {
 
-    @Value("${test.name}")
+    /**
+     * @Value("${test.name}")
     private String name;
 
     @Value("${test.age}")
@@ -17,10 +19,14 @@ public class SampleRunner implements ApplicationRunner {
     @Value("${test.fullName}")
     private String fullName;
 
+     */
+    @Autowired
+    MyProperties myProperties;
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(fullName);
+        System.out.println(myProperties.getName());
+        System.out.println(myProperties.getAge());
+        System.out.println(myProperties.getFullName());
+        System.out.println(myProperties.getSessionTimeout());
     }
 }

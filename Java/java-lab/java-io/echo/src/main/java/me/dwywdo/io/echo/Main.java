13875f9fcb19d5620a02960532afa6b24d208ed7
Euiyub Jung <euiyub.jung@linecorp.com>
2023-01-09 19:49:31 +0900

    Implement continuous communication server and multi-clients

--- Java/java-lab/java-io/echo/src/main/java/me/dwywdo/io/echo/Main.java
+++ Java/java-lab/java-io/echo/src/main/java/me/dwywdo/io/echo/Main.java
@@ -2,11 +2,12 @@ package me.dwywdo.io.echo;
 
 import java.io.IOException;
 
+import me.dwywdo.io.echo.server.EchoMultiServer;
 import me.dwywdo.io.echo.server.EchoServer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        EchoServer server = new EchoServer();
-        server.start(4444);
+        EchoMultiServer server = new EchoMultiServer();
+        server.start(5555);
     }
 }

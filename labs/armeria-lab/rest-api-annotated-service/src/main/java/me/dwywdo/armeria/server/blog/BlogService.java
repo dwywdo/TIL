e6bf2c53a8bd1af4117a1f615fcc7ff6251ad804
Euiyub Jung <euiyub.jung@linecorp.com>
2024-04-03 23:37:47 +0900

    Implement CREATE

--- labs/armeria-lab/rest-api-annotated-service/src/main/java/me/dwywdo/armeria/server/blog/BlogService.java
+++ labs/armeria-lab/rest-api-annotated-service/src/main/java/me/dwywdo/armeria/server/blog/BlogService.java
@@ -9,10 +9,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public final class BlogService {
     private final Map<Integer, BlogPost> blogPostMap = new ConcurrentHashMap<>();
+    private final Map<Integer, BlogPost> blogPosts = new ConcurrentHashMap<>();
 
     @Post("/blogs")
     @RequestConverter(BlogPostRequestConverter.class)
     public HttpResponse createPostBlog(BlogPost blogPost) {
+        blogPosts.put(blogPost.getId(), blogPost);
         return HttpResponse.ofJson(blogPost);
     }
 }

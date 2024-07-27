961d9d589f6e015eee9c8d382bc16841f4dfa1b5
Euiyub Jung <euiyub.jung@linecorp.com>
2024-07-27 14:11:19 +0900

    Event handler - use 'function component' way

--- hands-on/react-comment-app/src/chapter_08/ConfirmButton.jsx
+++ hands-on/react-comment-app/src/chapter_08/ConfirmButton.jsx
@@ -1,30 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ConfirmButton extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            "isConfirmed": false,
-        };
-    }
+function ConfirmButton(props) {
+    const [isConfirmed, setIsConfirmed] = useState(false);
 
-    handleConfirm = () => {
-        this.setState((prevState) => ({
-            isConfirmed: !prevState.isConfirmed,
-        }));
-    }
+    const handleConfirm = () => {
+        setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
+    };
 
-    render() {
-        return (
-            <button
-                onClick={this.handleConfirm}
-                disabled={this.state.isConfirmed}
-            >
-                {this.state.isConfirmed ? "Confirmed" : "Confirm"}
-            </button>
-        );
-    }
+    return (
+        <button onClick={handleConfirm} disabled={isConfirmed}>
+            {isConfirmed ? "Confirmed" : "Confirm"}
+        </button>
+    )
 }
 
 export default ConfirmButton;

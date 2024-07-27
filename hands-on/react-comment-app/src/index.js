364c56bda059695293cf7d8604e69b757bc8816f
Euiyub Jung <euiyub.jung@linecorp.com>
2024-07-28 01:00:54 +0900

    Implement Attendance Book (List and Key)

--- hands-on/react-comment-app/src/index.js
+++ hands-on/react-comment-app/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <LandingPage />
+      <AttendanceBook />
   </React.StrictMode>
 );
 

bf3569d59f7e936e2c83f68063bf01d2f4c3dbec
Euiyub Jung <euiyub.jung@linecorp.com>
2024-07-28 14:27:35 +0900

    Implement ProfileCard - Composition (+Containaton/Specialization)

---
+++ hands-on/react-comment-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="Dwywdo" backgroundColor="#4ea04e">
+            <p>Hi, this is dwywdo</p>
+            <p>I'm developing software using React</p>
+        </Card>
+    );
+}
+
+export default ProfileCard

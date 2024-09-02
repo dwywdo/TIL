1e9c95c7a6cc2e83c7601ddccc59f9fc8a2e6118
Euiyub Jung <euiyub.jung@linecorp.com>
2024-09-03 01:57:02 +0900

    Use different thread

--- labs/java-lab/reactive-streams/src/main/java/me/dwywdo/labs/java/reactive/PubSub.java
+++ labs/java-lab/reactive-streams/src/main/java/me/dwywdo/labs/java/reactive/PubSub.java
@@ -5,15 +5,19 @@ import static java.util.concurrent.Flow.Publisher;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
 import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.TimeUnit;
 
 public class PubSub {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Publisher <- Observable
         // Subscriber <- Observer
 
         final Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+        ExecutorService es = Executors.newCachedThreadPool();
 
         final Flow.Publisher<Integer> p = new Publisher() {
             @Override
@@ -23,18 +27,22 @@ public class PubSub {
                         new Subscription() {
                             @Override
                             public void request(long n) {
-                                try {
-                                    while (n-- > 0) {
-                                        if (it.hasNext()) {
-                                            subscriber.onNext(it.next());
-                                        } else {
-                                            subscriber.onComplete();
-                                            break;
+                                es.execute(() -> {
+                                    int i = 0;
+                                    try {
+                                        while (i++ < n) {
+                                            if (it.hasNext()) {
+                                                subscriber.onNext(it.next());
+                                            } else {
+                                                subscriber.onComplete();
+                                                break;
+                                            }
                                         }
+                                    } catch (RuntimeException e) {
+                                        subscriber.onError(e);
                                     }
-                                } catch (RuntimeException e) {
-                                    subscriber.onError(e);
-                                }
+
+                                });
                             }
 
                             @Override
@@ -50,7 +58,7 @@ public class PubSub {
 
             @Override
             public void onSubscribe(Subscription subscription) {
-                System.out.println("onSubscribe " + subscription);
+                System.out.println(Thread.currentThread().getName() + " onSubscribe " + subscription);
                 this.subscription = subscription;
                 subscription.request(1);
             }
@@ -58,7 +66,7 @@ public class PubSub {
             @Override
             public void onNext(Integer item) {
                 // Publisher가 데이터를 주면 Subscriber가 받으면서 이것으로 처리하기 위한 메서드
-                System.out.println("onNext " + item);
+                System.out.println(Thread.currentThread().getName() + " onNext " + item);
                 subscription.request(1);
             }
 
@@ -66,17 +74,19 @@ public class PubSub {
             public void onError(Throwable throwable) {
                 // Exception 발생시키지 않고, Exception에 해당하는 객체를 만들어서 onError를 통해 전달
                 // Try-catch 구문이 필요가 없다.
-                System.out.println("onError" + throwable);
+                System.out.println("onError " + throwable.getMessage());
             }
 
             @Override
             public void onComplete() {
-                System.out.println("onComplete");
+                System.out.println(Thread.currentThread().getName() + " onComplete");
                 // Publisher가 줄 데이터가 이제 없어서 완료되었다고 하는 것
             }
         };
 
         p.subscribe(s);
 
+        es.awaitTermination(10, TimeUnit.HOURS);
+        es.shutdown();
     }
 }

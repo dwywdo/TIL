561d69b90f9174a37a9953e83832ccae21ec014f
Euiyub Jung <euiyub.jung@linecorp.com>
2024-09-14 12:32:14 +0900

    Super Type Token - Introduction to Generic Class

--- labs/java-lab/supertype-token/src/main/java/me/dwywdo/labs/java/supertype_token/TypeToken.java
+++ labs/java-lab/supertype-token/src/main/java/me/dwywdo/labs/java/supertype_token/TypeToken.java
@@ -8,6 +8,18 @@ public class TypeToken {
     static <T> T createWithGenerics(Class<T> clazz) throws InstantiationException, IllegalAccessException {
         return clazz.newInstance();
     }
+
+    static class Generic<T> {
+        private T value;
+
+        void set(T value) {
+            this.value = value;
+        }
+
+        T get() {
+            return value;
+        }
+    }
     
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         final Object o = create();
@@ -18,5 +30,17 @@ public class TypeToken {
         // final Integer i = createWithGenerics(Integer.class); 실행이 되지 않는다. Integer 클래스에는 Integer() Default 생성자가 없기 때문.
         System.out.println("s.getClass() = " + s.getClass());
         System.out.println("o2.getClass() = " + o2.getClass());
+
+
+        // final Generic<String> sGeneric = new Generic<String>(); -> Diamond 타입으로 생략할 수 있다.
+        final Generic<String> sGeneric = new Generic<>();
+        sGeneric.set("sGeneric String");
+        System.out.println("sGeneric.value = " + sGeneric.value);
+
+        final Generic<Integer> iGeneric = new Generic<>();
+        iGeneric.set(10);
+        System.out.println("iGeneric.value = " + iGeneric.value);
+
+        System.out.println("iGeneric.value.getClass() = " + iGeneric.value.getClass());
     }
 }
